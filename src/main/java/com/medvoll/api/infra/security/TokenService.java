1e3df2cd436ae50fbcd56c7568362336b7354bd7
Santiago Ferreira <santiandresfer@hotmail.com>
2025-08-04 16:50:02 -0300

    Manejando clave secreta para firma del token

--- src/main/java/com/medvoll/api/infra/security/TokenService.java
+++ src/main/java/com/medvoll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.medvoll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -11,9 +12,12 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}") // Clave secreta para firmar el token, definida en application.properties
+    private String secret;
+
     public String generarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.HMAC256("12345678");
+            var algoritmo = Algorithm.HMAC256(secret); // Clave secreta para firmar el token
             return JWT.create()
                     .withIssuer("API Vollmed") // Sistema que emite el token
                     .withSubject(usuario.getLogin())
