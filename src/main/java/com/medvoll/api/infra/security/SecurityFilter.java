664a16dc473312437f420ce012cb5e761089feed
Santiago Ferreira <santiandresfer@hotmail.com>
2025-08-05 15:29:48 -0300

    Recuperando token, aplicando filtros y validandolo

--- src/main/java/com/medvoll/api/infra/security/SecurityFilter.java
+++ src/main/java/com/medvoll/api/infra/security/SecurityFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
 @Component
@@ -13,7 +12,17 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
 
         filterChain.doFilter(request, response); // Permite que la solicitud continúe su camino en la cadena de filtros
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("No se envió el token JWT en el encabezado Authorization");
+        }
+        return authorizationHeader.replace("Bearer ", ""); // Elimina el prefijo "Bearer " del token
     }
 }
