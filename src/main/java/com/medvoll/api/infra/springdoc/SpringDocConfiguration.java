35aa30a1328cb2b1aff9087039b3c4f8ce47e1b3
Santiago Ferreira <santiandresfer@hotmail.com>
2025-08-10 01:11:15 -0300

    Agrego información de la api en Spring-doc

--- src/main/java/com/medvoll/api/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/com/medvoll/api/infra/springdoc/SpringDocConfiguration.java
@@ -1,6 +1,8 @@
 package com.medvoll.api.infra.springdoc;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,12 @@ public class SpringDocConfiguration {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                        .info(new Info()
+                .title("Vollmed API")
+                .description("API Rest de la aplicación Vollmed, que contiene las funcionalidades CRUD de médicos y de pacientes, además de reserva y cancelamiento de consultas")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://voll.med/api/licencia")));
     }
 }
