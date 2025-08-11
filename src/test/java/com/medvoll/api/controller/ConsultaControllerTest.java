56a5b487b50878857504ceeb9fdcf867ffbeb3eb
Santiago Ferreira <santiandresfer@hotmail.com>
2025-08-11 19:08:00 -0300

    Primer Test de ConsultaController corroborando retorno de código de estado 400

---
+++ src/test/java/com/medvoll/api/controller/ConsultaControllerTest.java
@@ -0,0 +1,32 @@
+package com.medvoll.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc // Configura MockMvc para pruebas de controladores
+class ConsultaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Debería devolver código HTTP 400 cuando la request no contiene datos")
+    @WithMockUser // Simula un usuario autenticado para las pruebas
+    void agendarConsulta_escenario1() throws Exception {
+        var response = mvc.perform(post("/consultas"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
